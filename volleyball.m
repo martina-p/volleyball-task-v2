91583a0bf79d59976fdcd874ee05aa206cf3fc5f
martina-p <martina.puppi@gmail.com>
2017-06-20 00:17:17 +0200

    end of changes to version2 of the task

--- volleyball.m
+++ volleyball.m
@@ -13,36 +13,36 @@ end
 resultname = fullfile('Logfiles', strcat('Sub',num2str(subjectID),'_', DateTime, '.mat'));                          %for choices
 resultnameQuestions = fullfile('Logfiles', strcat('Sub',num2str(subjectID),'questions_', DateTime, '.mat'));        %for end-of-block questions
 backupfile = fullfile('Logfiles', strcat('Bckup_Sub',num2str(subjectID), '_', DateTime, '.mat'));                   %backup logfile with the whole workspace, in case of nasty aborts
-%% ========= SET CONTINGENCIES & EXPERIMENT STRUCTURE ========= %
+%% ========= SET CONTINGENCIES ========= %
 T = readtable('ContingencyTableFinal.csv');         %read table that stores the 30 conditions with their contingencies and learning duration
 shuffledT = T(randperm(size(T,1)),:);               %shuffle T rows (ie shuffle the 30 conditions)
-condition = shuffledT.Condition;                    %pick rows for indexing
 
 %Create contingency tables for different number of trials
-poa4 = shuffledT.POA4;
+poa4 = shuffledT.POA4; %4 trials
 ponota4 = shuffledT.POnotA4;
 contTable4 = [poa4, ponota4];
 
-poa5 = shuffledT.POA5;
+poa5 = shuffledT.POA5; %5 trials
 ponota5 = shuffledT.POnotA5;
 contTable5 = [poa5, ponota5];
 
-poa6 = shuffledT.POA6;
+poa6 = shuffledT.POA6;  %6 trials
 ponota6 = shuffledT.POnotA6;
 contTable6 = [poa6, ponota6];
 
-poa38 = shuffledT.POA38;
+poa38 = shuffledT.POA38; %38 trials
 ponota38 = shuffledT.POnotA38;
 contTable38 = [poa38, ponota38];
 
-poa40 = shuffledT.POA40;
+poa40 = shuffledT.POA40;  %40 trials
 ponota40 = shuffledT.POnotA40;
 contTable40 = [poa40, ponota40];
 
-poa42 = shuffledT.POA42;
+poa42 = shuffledT.POA42;    %42 trials
 ponota42 = shuffledT.POnotA42;
 contTable42 = [poa42, ponota42];
 
+%% ========= EXPERIMENT STRUCTURE ========= %
 run1 = shuffledT{1:10,'LearnDur'};            %determine the duration (short or long) for each block in run1 (0 short, 1 long)         
 run2 = shuffledT{11:20,'LearnDur'};           %determine the duration (short or long) for each block in run2 (0 short, 1 long)
 run3 = shuffledT{21:30,'LearnDur'};           %determine the duration (short or long) for each block in run3 (0 short, 1 long)
@@ -50,16 +50,11 @@ training = 10;                                %determine the duration (10 trials
 expStructure = {training, run1, run2, run3};
 nruns = numel(expStructure);                  %count elements in expStructure to determine number of runs
 
-%% ========= OTHER PARAMETERS & DATA PREALLOCATION ========= %
-%Block structure
 shortBlocks = [4 5 6];
 longBlocks = [38 40 42];
 
-cond = [0 0 0 0 0 1 1 1 1 1];                                     %for play_pause or pause_play display, changes every block
-players = randperm(30+1);                                         %create as many unique "player numbers" as there are blocks, + 1 for the practice run
-thisblock = zeros(310,1);                                         %preallocate block nr storage to be recorded for each trial
-stimOrder = zeros(310,1);                                         %preallocate condition (play_pause or pause_play) to be recorded for each trial
-
+cond = [0 0 0 0 0 1 1 1 1 1];                   %for play_pause or pause_play display
+players = randperm(30+1);                       %create as many unique "player numbers" as there are blocks, + 1 for the practice run
 %% ========= INSTRUCTIONS ========= %
 psychExpInit;                                %start PTB
 RestrictKeysForKbCheck([32,37,39]);          %restrict key presses to space, right and left arrows
@@ -102,18 +97,29 @@ while exitInstructions == false              %loop instruction slides until spac
 end
 
 %% ========= LOOPS (RUN, BLOCK, TRIAL) ========= %
-runnb = 0;   %initial run value
-blocknb = 0; %initial block value
+totalCount = 0;     %this will be equal to the total nr of trials in the end
+runnb = 0;          %initial run value     
 
 %RUN LOOP
 for i = 1:nruns
-    condOrder = cond(randperm(length(cond)));   %determine play_pause or pause_play condition
+    blocknb = 0;
+    runnb = runnb + 1;
+    condOrder = cond(randperm(length(cond)));   %determine play_pause or pause_play condition sequence
     nblocks = length(expStructure{i});          %determine number of blocks for each run; it will be 1 for training and 10 for the other 3 runs
-
+    
+    if i == 2 %if this is the first run, start adding up to blocknb from 0
+        blocknb = 0;
+    elseif i == 3 %if this is the second run, start adding up to blocknb from 10
+        blocknb = 10;
+    elseif i == 4 %if this is the third run, start adding up to blocknb from 20
+        blocknb = 20;
+    end    
+    
 %BLOCK LOOP
-for x = 1:nblocks          
+for x = 1:nblocks  
         blocknb = blocknb + 1;
-          
+        stimOrder = condOrder(x); %to store stim order (see later: 0 play_pause, 1 pause_play)
+                
         %Determine exact number of trials (4, 5, 6 or 38, 40, 42) depending
         %on learning duration of condition
         if expStructure{i}(x,1) == 0 
@@ -153,7 +159,14 @@ for x = 1:nblocks
             P_OA = contTable42(blocknb,:);
         end
         
-        respEndOfBlock = {length(ntrials),4};                      %prealocate responses to end-of-block questions
+        if i >= 2
+           thisConditionT = shuffledT(blocknb, {'Condition'}); %keep track of the condition for this block (after practice run)
+           thisCondition = table2array(thisConditionT);
+           thisDPTable = shuffledT(blocknb, {'DP'});
+           thisDP = table2array(thisDPTable);    %keep track of DP for this block (after practice run)
+        end
+        
+        respEndOfBlock = {length(ntrials),4}; %prealocate responses to end-of-block questions
         
         %Set pseudo-random sequence of outcomes
         nresp = 2;
@@ -187,21 +200,15 @@ for x = 1:nblocks
         while k <= ntrials                                      %use while instead of for loop to accomodate late trials
             trialnb = 0;
             trialnb = trialnb + 1;
-            thistrial(trialnb,1) = k;                           %store number of trial
-            thisblock(trialnb,1) = blocknb;                     %store block nr
-            stimOrder(trialnb,1) = condOrder(:,x);              %store stimulus order (play_pause or pause_play)
-            %condition(trialnb,1) = condition(x,1);              %store condition number 1-30
-            %P_OA(trialnb,1) = P_OA(:,1);                       %store P_OA
-            %thisP_OnotA(trialnb,1) = P_OA(:,2);                %store P_OnotA
-            %thisdeltaP(trialnb,1) = deltaP;
+            totalCount = totalCount + 1;
             save(backupfile)                                    % backs the entire workspace up just in case we have to do a nasty abort
             RestrictKeysForKbCheck([27,37,39]);                 %restrict key presses to right and left arrows
             
             %Draw stimuli play / do not play
-            if condOrder(:,x)==0   
+            if condOrder(x)==0   
                 Screen('DrawTexture', win, texPlay,[],imageRectPlayLeft);
                 Screen('DrawTexture', win, texPause,[],imageRectPauseRight);
-            elseif condOrder(:,x)==1
+            elseif condOrder(x)==1
                 Screen('DrawTexture', win, texPlay,[],imageRectPlayRight);
                 Screen('DrawTexture', win, texPause,[],imageRectPauseLeft);
             end
@@ -218,10 +225,10 @@ for x = 1:nblocks
                 
                 %Spot late trials & keep track of them for later
                 if GetSecs>maxStimDuration
-                    if condOrder(:,x)==0   
+                    if condOrder(x)==0   
                         Screen('DrawTexture', win, texPlay,[],imageRectPlayLeft);
                         Screen('DrawTexture', win, texPause,[],imageRectPauseRight);
-                    elseif condOrder(:,x)==1
+                    elseif condOrder(x)==1
                         Screen('DrawTexture', win, texPlay,[],imageRectPlayRight);
                         Screen('DrawTexture', win, texPause,[],imageRectPauseLeft);
                     end                    
@@ -270,11 +277,23 @@ for x = 1:nblocks
             nLateTrials = numel(find(lateTrials(:,1)==1)); %count how many late trials there have been
             nLateTrialsThisBlock(trialnb,1) = nLateTrials; %this is for adding it to data table
             
-            %Store iterations
-            reactionTimes(trialnb,1) = reactionTime;
-            choices(trialnb,1) = n;
-            outcomes(trialnb,1) = outcome;
             k=k+1;
+            
+            %Store variables to be saved
+            data.subjID(totalCount) = subjectID;
+            data.run(totalCount) = runnb;
+            data.block(totalCount) = blocknb;
+            if i >= 2
+                data.condition(totalCount) = thisCondition;
+                data.DP(totalCount) = thisDP;
+            end
+            data.blockLenght(totalCount) = ntrials;
+            data.trial(totalCount) = totalCount;
+            data.stimOrder(totalCount) = stimOrder;
+            data.subjChoice(totalCount) = n;
+            data.outcome(totalCount) = outcome;
+            data.rt(totalCount) = reactionTime;
+            
         end
               
 %% ========= END-OF-BLOCK QUESTIONS ========= %
@@ -307,14 +326,16 @@ if i == 1 && x == 1
     [secs, keyCode, deltaSecs] = KbWait([],2);  %wait forkey press (self-paced start after practice session)
 elseif i == 4    
     %calculate earning 
-    %pickblock = randsample(2:31,1);                                         %pick a random block b/w 2 and 31 (1 is training)
-    %score = abs(thisdeltaP(pickblock) - respEndOfBlock{pickblock,1});       %look up that block's respQ1 and subtract it from that block's deltaP
-    %earning = 5+(0.1*(10-score).^2);                                        %plug score in payment equation and add 5 show-up fee
+    pickblock = randsample(1:30,1);                                     %pick a random block
+    DPpayment = shuffledT(pickblock, {'DP'});                           %determine the DP for that block
+    thisDPpayment = table2array(DPpayment);                             %extract it from the table
+    score = abs(thisDPpayment - respEndOfBlock{pickblock,1});           %look up that block's respQ1 and subtract it from that block's deltaP
+    earning = 5+(0.1*(10-score).^2);                                    %plug score in payment equation and add 5 show-up fee
     %display earning
-    %DrawFormattedText(win,'FINE DEL GIOCO \n \n Grazie della partecipazione. \n \n Hai vinto Euro:','center','center',white);
-    %DrawFormattedText(win,num2str(earning),800,800,white);
-    %Screen('Flip',win);
-    %WaitSecs(3);
+    DrawFormattedText(win,'FINE DEL GIOCO \n \n Grazie della partecipazione. \n \n Hai vinto Euro:','center','center',white);
+    DrawFormattedText(win,num2str(earning),800,800,white);
+    Screen('Flip',win);
+    WaitSecs(3);
 else
     RestrictKeysForKbCheck([32]);               %restrict key presses to space
     DrawFormattedText(win,'PAUSA \n \n Premi SPAZIO quando sei pronto a ricominciare.','center','center',white);
@@ -325,10 +346,12 @@ end
 end
        
 %% ========= SAVE DATA & CLOSE ========= %
-subject(1:trialnb,1) = subjectID;
-data = [subject, thisblock, stimOrder, thistrial, choices, outcomes, reactionTimes, nLateTrialsThisBlock];
+Data = struct2table(data);
+save(resultname, 'Data');
+
 dataQuestions = (respEndOfBlock);
 save(resultnameQuestions, 'dataQuestions');
-save(resultname, 'data');
 
+%Close screen and escape
 Screen('CloseAll');
+sca;
